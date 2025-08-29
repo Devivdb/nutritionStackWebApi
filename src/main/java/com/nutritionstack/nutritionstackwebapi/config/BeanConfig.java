e967d213ffb0252912be904ed20fb5eb35abe8a6
Devivdb <devivdb@gmail.com>
2025-08-29 14:45:27 +0200

    feat: Configure Spring Security and authentication endpoints

---
+++ src/main/java/com/nutritionstack/nutritionstackwebapi/config/BeanConfig.java
@@ -0,0 +1,21 @@
+package com.nutritionstack.nutritionstackwebapi.config;
+
+import com.nutritionstack.nutritionstackwebapi.service.JwtService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public JwtService jwtService() {
+        return new JwtService();
+    }
+}
