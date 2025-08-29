4dcb13111cda58b327ed5867a10bb1a76dfceebf
Devivdb <devivdb@gmail.com>
2025-08-29 12:35:06 +0200

    first commit

---
+++ src/main/java/com/nutritionstack/nutritionstackwebapi/NutritionStackWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutritionstack.nutritionstackwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionStackWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionStackWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutritionstack/nutritionstackwebapi/NutritionStackWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritionstack.nutritionstackwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionStackWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
