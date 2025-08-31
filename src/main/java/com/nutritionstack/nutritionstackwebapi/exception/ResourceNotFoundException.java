fbbdc94339213c60cffd4df66f02bd40fe10bc98
Devivdb <devivdb@gmail.com>
2025-08-31 21:47:59 +0200

    feat: improve error handling and meal type validation

---
+++ src/main/java/com/nutritionstack/nutritionstackwebapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.nutritionstack.nutritionstackwebapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
